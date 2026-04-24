0f3c5748fa0dd15ac19213a54684116cbea48848
Petrushevskiy Maxim <maxinverst@gmail.com>
2026-04-24 17:19:08 +0500

    feat: add aside and main content html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-import './style.css';
+import 'normalize.css';
+import './style/style.css';
+
+const asideBar = document.querySelector('.aside');
+const openAsideButton = document.querySelector('.open-aside-btn');
+
+function toggleAside() {
+    asideBar.classList.toggle('hidden');
+    openAsideButton.classList.toggle('hidden');
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.closest('.aside__button') || e.target.closest('.open-aside-btn')) {
+        toggleAside();
+    }
+});
+
+localStorage.setItem('monthlyData', JSON.stringify({}));
+
+const dateNow = new Date();
+const year = dateNow.getFullYear();
+const month = dateNow.getMonth();
+
+// console.log( `${year}-${month}`);
