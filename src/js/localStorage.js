03ca9e6c47699a0ae7adad9fb1e522384f96d5c6
Petrushevskiy Maxim <maxinverst@gmail.com>
2026-04-26 19:38:26 +0500

    refactor: refactor localStorage logic

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,26 +1,31 @@
+import { renderTableProjects } from './renderTableProjects';
+
 const LOCAL_STORAGE_KEY = 'monthlyDate';
 
 export const saveProjectToLocalStorage = (data) => {
-    const dateNow = new Date();
-    const year = dateNow.getFullYear();
-    const month = dateNow.getMonth();
-    const dateKey = `${year}-${month}`;
-
-    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-    const storageData = storage ? JSON.parse(storage) : {};
-    console.log(storageData);
-    console.log(data);
-
     if (!data) return;
+    const dateKey = getDateKey();
+
+    const project = {
+        id: new Date().getTime(),
+        projectName: data['project-name'],
+        companyName: data['company-name'],
+        budget: data.budget,
+        employeeCapacity: data['employee-capacity'],
+        employees: [],
+    };
+
+    const storageData = getLocalStorage();
 
     if (
         storageData[dateKey]?.projects &&
         storageData[dateKey]?.projects.length > 0 &&
         Array.isArray(storageData[dateKey]?.projects)
     ) {
-        storageData[dateKey].projects.push(data);
+        storageData[dateKey].projects.push(project);
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(storageData));
+        renderTableProjects(storageData[dateKey].projects);
+
         return;
     }
 
@@ -29,7 +34,24 @@ export const saveProjectToLocalStorage = (data) => {
         projects: [],
     };
 
-    storageData[dateKey].projects.push(data);
+    storageData[dateKey].projects.push(project);
 
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(storageData));
+
+    renderTableProjects(storageData[dateKey].projects);
 };
+
+export function getLocalStorage() {
+    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    const storageData = storage ? JSON.parse(storage) : {};
+    return storageData;
+}
+
+function getDateKey() {
+    const dateNow = new Date();
+    const year = dateNow.getFullYear();
+    const month = dateNow.getMonth();
+    const dateKey = `${year}-${month}`;
+    return dateKey;
+}
